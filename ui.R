ff312c1977e3b75801353c7c900e5fe6f5bbc73b
muschellij2 <muschellij2@gmail.com>
2014-03-15 16:40:26 -0400

    Removed cancelled session bug

--- ui.R
+++ ui.R
@@ -20,7 +20,7 @@ shinyUI(pageWithSidebar(
                 choices= time, selected=time[1]),   
     selectInput("drop_sess", "Session:",
                 choices= sess, selected=sess[1]),  
-    textInput("search", "Search Criteria", ""),
+    textInput("search", "Search Criteria (uses grep):", ""),
     downloadButton("dlcsv", "Download CSV of Table"),
     HTML('<br>If you like it donate a beer at ENAR or PayPal:
 <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=muschellij2%40gmail%2ecom&lc=US&item_name=ENAR%20Scraper&item_number=enar_scraper2014&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
